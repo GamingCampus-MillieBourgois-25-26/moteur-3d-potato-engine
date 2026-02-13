8948d7a0227f27f6424a6899d9432555d541a9ca
Toupetit2 <theo.doublet@yahoo.com>
2026-02-13 01:30:00 +0100

    Initialize Jolt physics system and members

--- source/Physics/physicsSystem.cpp
+++ source/Physics/physicsSystem.cpp
@@ -1,9 +1,8 @@
 #include "physicsSystem.h"
 
-#include "physicsLayerManager.h"
-
 #include <Jolt/Jolt.h>
 #include <Jolt/RegisterTypes.h>
+#include <Jolt/Core/TempAllocator.h>
 
 
 void PhysicsSystem::Init()
@@ -12,14 +11,19 @@ void PhysicsSystem::Init()
 
 	JPH::RegisterTypes();
 
-	PhysicsLayerManager physicsLayerManager;
+	m_system.Init(cMaxBodies, cNumBodyMutexes, cMaxBodyPairs, cMaxContactConstraints, nullptr, &m_layerManager, nullptr);
 
-	m_system.Init(cMaxBodies, cNumBodyMutexes, cMaxBodyPairs, cMaxContactConstraints, nullptr, &physicsLayerManager, nullptr)
+	m_system.SetGravity(JPH::Vec3(0.0f, -9.81f, 0.0f));
+
+	m_system.SetContactListener(&m_contactListener);
+
+	
 }
 
 void PhysicsSystem::Update(float deltaTime)
 {
-	
+	// TODO
+	//m_system.Update(cDeltaTime, cCollisionSteps, &temp_allocator, ???);
 }
 
 JPH::PhysicsSystem& PhysicsSystem::GetJoltSystem()
--- source/Physics/physicsSystem.h
+++ source/Physics/physicsSystem.h
@@ -2,6 +2,9 @@
 #include <Jolt/Jolt.h>
 #include <Jolt/Physics/PhysicsSystem.h>
 
+#include "physicsLayerManager.h"
+#include "physicsContactListener.h"
+
 class PhysicsSystem
 {
 public:
@@ -13,8 +16,16 @@ public:
 private:
 	JPH::PhysicsSystem m_system;
 
+	JPH::TempAllocatorImpl temp_allocator = JPH::TempAllocatorImpl(10 * 1024 * 1024);
+	// Managers / listeners stockés ici pour garantir leur durée de vie
+	PhysicsLayerManager m_layerManager;
+	PhysicsContactListener m_contactListener;
+
 	const JPH::uint cMaxBodies = 65536;
 	const JPH::uint cNumBodyMutexes = 0;
 	const JPH::uint cMaxBodyPairs = 65536;
 	const JPH::uint cMaxContactConstraints = 10240;
+
+	const float cDeltaTime = 1.0f / 60.0f; // 60 ticks par seconde
+	const int cCollisionSteps = 1;
 };
