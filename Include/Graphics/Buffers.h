cf5b2f21c404b8273e7c52e1be9ffca9e2536a4f
Poisskaille <adrien.moreau2500@gmail.com>
2026-02-23 11:13:09 +0100

    .

--- Include/Graphics/Buffers.h
+++ Include/Graphics/Buffers.h
@@ -4,7 +4,14 @@
 
 class Buffers
 {
+private:
+	static Buffers* instance;
+
 public:
 	std::unordered_map<std::string,std::shared_ptr<Mesh>> meshBuffer;
 
+	void AddMeshBuffers(std::shared_ptr<Mesh>);
+	static Buffers* getInstance();
 };
+
+extern Buffers* buffer;
--- source/Graphics/Parser/Buffers.cpp
+++ source/Graphics/Parser/Buffers.cpp
@@ -1,1 +1,16 @@
 #include "../include/Graphics/Buffers.h"
+
+void Buffers::AddMeshBuffers(std::shared_ptr<Mesh> mesh)
+{
+	meshBuffer.insert();
+}
+
+Buffers* Buffers::getInstance()
+{
+	if (instance == nullptr)
+		instance = new Buffers();
+	return instance;
+}
+
+Buffers* Buffers::instance = nullptr;
+Buffers* buffer = Buffers::getInstance();
